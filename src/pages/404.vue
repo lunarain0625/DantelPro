35a7a90105b4b73e4a99aca1329004d7e87d9567
Harry YYDS <harry.wang@ielts-daily.com>
2024-07-12 17:40:16 +1000

    2

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-  <h1>404</h1>
+  <h1 class="text-slate-500">404</h1>
+  <div class="text-xl font-medium text-red-600">ChitChat</div>
+  <div class="text-xl font-medium text-red-100">ChitChat</div>
+  <div class="text-blue-100">ChitChat</div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
